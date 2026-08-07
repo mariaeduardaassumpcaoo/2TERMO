20bfbb2f87c791a9a89892f40c206ba1e5482ce9
mariaeduardaassumpcaoo <mariaeduardaassumpcao56@gmail.com>
2026-08-07 15:16:20 -0300

    atualizar

--- PBE1/Aula03/balada.js
+++
@@ -1,8 +0,0 @@
-const entrada = require('readline-sync');
-const idade = entrada.questionInt('Qual a sua idade? ');
-
-if (idade >=18) {
-    console.log("Acesso liberado! Divirta-se.");
-} else {
-    console.log("Acesso negado! Vá para casa estudar Node.js.");
-}
