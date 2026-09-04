3c572a6aab6b39afa038ff9d3cf330df09e843af
mariaeduardaassumpcaoo <mariaeduardaassumpcao56@gmail.com>
2026-09-04 13:35:44 -0300

    commit

--- PBE1/Aula/aulas/array.js
+++
@@ -1,9 +0,0 @@
-// Criando o prédio com 3 moradores
-const moradores = ["Ana", "Bruno", "Carlos"];
-
-// Como o computador "enxerga"
-// moradores[0] -> "Ana"
-// moradores[1] -> "Bruno"
-// moradores[2] -> "Carlos"
-
-console.log(moradores[1]); // Vai imprimir Bruno
