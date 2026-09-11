9283ef2037557ac54605116b79e75881838b851d
mariaeduardaassumpcaoo <mariaeduardaassumpcao56@gmail.com>
2026-09-11 07:43:08 -0300

    atualizar

---
+++ 2TERMO/PBE1/atividadeapresentação/aluno.js
@@ -0,0 +1,14 @@
+// Exemplo: Sistema Escolar - Objeto Aluno com atributos
+const aluno = {
+    nome: "Maria Eduarda",
+    idade: 16,
+    matricula: 20260077
+};
+
+// Acessando os atributos do objeto
+console.log(`Nome: ${aluno.nome}`);
+console.log(`Idade: ${aluno.idade}`);
+console.log(`Matricula: ${aluno.matricula}`);
+
+
+
